1f3161e531288689bde92375a621201db94017ef
AyeshaShahzaib <ayeshashahzaib007@gmail.com>
2024-09-20 14:06:55 +0500

    I remove the double quotes

--- landing_page/src/app/Components/HeroSection.js
+++ landing_page/src/app/Components/HeroSection.js
@@ -5,10 +5,10 @@ export default function HeroSection(){
         <div className={style.video_container}>
         <video src="/main_video.mp4" autoplay muted loop preload="autoplay" className={style.video}></video>
         <div className={style.overlay}>
-                    <div className={style.SubTitle}><h2><em>"Join the Future of Technology"</em></h2></div>
+                    <div className={style.SubTitle}><h2><em>Join the Future of Technology</em></h2></div>
                    <div className={style.p}><p>
-                    "Join us at the tech event of the year! Explore groundbreaking innovations, connect with industry leaders, and gain insights from top experts. Secure your spot now and be part of the future of technology."</p></div>
-                    <div className={style.secondSubTitle}><p className={style.secondSubTitle}><b> "Innovate, Inspire, Impact"</b></p></div>
+                    Join us at the tech event of the year! Explore groundbreaking innovations, connect with industry leaders, and gain insights from top experts. Secure your spot now and be part of the future of technology.</p></div>
+                    <div className={style.secondSubTitle}><p className={style.secondSubTitle}><b> Innovate, Inspire, Impact</b></p></div>
                     <div className={style.Button}><button><b>Join Now</b></button></div>
                 </div>
       </div> 
--- landing_page/src/app/Components/card_of_about_us.js
+++ landing_page/src/app/Components/card_of_about_us.js
@@ -12,15 +12,15 @@ export default function AboutUS() {
           <div className={style.overview}>
             <h2>Overview</h2>
             <p>
-              "The Tech Conference 2024 brings together leading innovators,
+              The Tech Conference 2024 brings together leading innovators,
               industry experts, and technology enthusiasts for a dynamic exploration
               of the future of technology. Our goal is to empower attendees with
-              knowledge, connections, and inspiration to shape tomorrow’s tech landscape."
+              knowledge, connections, and inspiration to shape tomorrows tech landscape.
             </p>
           </div>
           <div className={style.our_mission}>
             <h2>Our Mission</h2>
-            <p>"Our mission is to inspire and equip attendees to push the boundaries of technology, drive innovation in their respective fields, and make meaningful contributions to the future of tech."</p>
+            <p>Our mission is to inspire and equip attendees to push the boundaries of technology, drive innovation in their respective fields, and make meaningful contributions to the future of tech.</p>
           </div>
         </div>
       </div>
