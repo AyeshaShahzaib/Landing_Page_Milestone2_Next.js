f67fcafc853a7959bc70e184b52c9d3aad0a217c
AyeshaShahzaib <ayeshashahzaib007@gmail.com>
2024-09-20 14:42:37 +0500

    change in folder name

--- landing_page/src/app/index.js
+++ landing_page/src/app/index.js
@@ -1,7 +1,7 @@
-import Home from './Home page/home'
-import Features from './Home page/features'
-import About_Us from './Home page/Aboutus'
-import ContactUs from './Home page/Contact'
+import Home from './pages/home'
+import Features from './pages/features'
+import About_Us from './pages/Aboutus'
+import ContactUs from './pages/Contact'
 export default function Landing_Page(){
     return(
         <>
